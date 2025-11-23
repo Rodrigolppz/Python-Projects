276c1df194672ed7b1a97606ea3e917b8407a597
Rodrigolppz <rodrigobracens@gmail.com>
2025-11-23 13:56:06 -0300

    code

---
+++ number_guesser/number_guesser.py
@@ -0,0 +1,4 @@
+# Generate a random number and track how many times the user takes to get this number correctly
+
+import random
+
--- quiz_game/quiz-game.py
+++ quiz_game/quiz-game.py
@@ -1,3 +1,34 @@
-# Create a program that asks user a bunch of questions, then if they answer the right thing, add 1 to their score and at the end of the program print their score on the screen.
+#Create a program that asks user a bunch of questions, then if they answer the right thing, add 1 to their score and at the end of the program print their score on the screen.
+
+score = 0
+
+answer = input("Whats the name of the program creator? ")
+if answer == "Rodrigo":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")
+
+answer = int(input("How many fingers a human hand has? "))
+if answer == 5:
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("CPU stands for ")
+if answer == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("christmas is good yes or not? ")
+if answer == "yes":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
 
 
+print("Your score is ", score,"/4")
